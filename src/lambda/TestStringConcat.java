0eb0e97c1620de2b15e09cd7390ca50fdf3cbb25
bbssm <bbssm@DESKTOP-3K7KAFU>
2022-02-08 23:48:37 +0900

    good day

---
+++ src/lambda/TestStringConcat.java
@@ -0,0 +1,13 @@
+package lambda;
+
+public class TestStringConcat {
+
+	public static void main(String[] args) {
+		String s1 = "Hello";
+		String s2 = "World";
+		StringConcatimpl concat1 = new StringConcatimpl();
+		concat1.makeString(s1, s2);
+
+	}
+
+}
